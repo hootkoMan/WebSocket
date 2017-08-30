da11026ae4311d3a875a13b3fdcf08d314c79e8e
Ruslan <ruslan.bur@gmail.com>
2017-08-30 13:36:38 +0300

    create project

---
+++ src/main/java/test/socket/demo/Greeting.java
@@ -0,0 +1,12 @@
+package test.socket.demo;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+public class Greeting {
+    private String content;
+}
